84005ee66d20c595be26d26586143269dd5a884e
Haru <64423645+harulol@users.noreply.github.com>
2021-08-07 19:26:39 +0900

    No longer shadowing because GUIs don't work if shaded.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,6 @@
-import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
-
 plugins {
     java
     kotlin("jvm") version "1.5.21"
-    id("com.github.johnrengelman.shadow") version "7.0.0"
 }
 
 setProperty("libsDirName", name)
@@ -20,13 +17,12 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib"))
     implementation("org.bukkit:bukkit:1.8-R0.1-SNAPSHOT")
-    implementation("dev.hawu.plugins:HikariLibrary:0.5.0-beta")
+    implementation("dev.hawu.plugins:HikariLibrary:0.5.1-beta")
     implementation("org.jetbrains:annotations:21.0.1")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
 
-configurations.getByName("implementation").isCanBeResolved = true
 var fileName: String? = null
 
 fun computeFileName(): String {
@@ -75,51 +71,27 @@ tasks.getByName<Test>("test") {
     useJUnitPlatform()
 }
 
-tasks.getByName<ShadowJar>("shadowJar") {
-    minimize()
-    exclude("META-INF/INDEX.LIST", "META-INF/*.SF", "META-INF/*.DSA", "META-INF/*.RSA")
-    configurations = listOf(project.configurations.getByName("implementation"))
-    dependencies {
-        include(dependency(dependencies.kotlin("stdlib")))
-        include(dependency("dev.hawu.plugins:HikariLibrary:.*"))
-        exclude(dependency("org.bukkit:bukkit:.*"))
-        exclude(dependency("org.jetbrains:annotations:.*"))
-    }
-    relocate("dev.hawu.plugins.api", "dev.hawu.plugins.queuedsudo.libs.api")
-    relocate("kotlin", "dev.hawu.plugins.queuedsudo.libs.kotlin")
-    
-    group = "shadow"
-    description = "An uber jar that does not need any external dependencies."
-    archiveFileName.set("${computeFileName()}-all.jar")
-    conventionMapping.map("classifier") {
-        "all"
-    }
-    
-    manifest.inheritFrom(project.tasks.getByName<Jar>("jar").manifest)
-}
-
-tasks.create<ShadowJar>("no-lib") {
-    minimize()
-    exclude("META-INF/INDEX.LIST", "META-INF/*.SF", "META-INF/*.DSA", "META-INF/*.RSA")
-    configurations = listOf(project.configurations.getByName("implementation"))
-    dependencies {
-        exclude(dependency(dependencies.kotlin("stdlib")))
-        include(dependency("dev.hawu.plugins:HikariLibrary:.*"))
-        exclude(dependency("org.bukkit:bukkit:.*"))
-        exclude(dependency("org.jetbrains:annotations:.*"))
-    }
-    relocate("dev.hawu.plugins.api", "dev.hawu.plugins.queuedsudo.libs.api")
-    
-    group = "shadow"
-    description = "A jar that contains this project's classes and the library that requires Kotlin to run."
-    archiveFileName.set("${computeFileName()}-noLib.jar")
-    conventionMapping.map("classifier") {
-        "all"
-    }
-    from(tasks.getByName<Jar>("jar").outputs)
-    manifest.inheritFrom(project.tasks.getByName<Jar>("jar").manifest)
-}
-
 tasks.getByName<Jar>("jar") {
+    ant {
+        withGroovyBuilder {
+            "replace"(
+                "file" to "src/main/resources/plugin.yml",
+                "token" to "@VERSION@",
+                "value" to computeFileName(),
+            )
+        }
+    }
     archiveFileName.set("${computeFileName()}.jar")
+    
+    doLast {
+        ant {
+            withGroovyBuilder {
+                "replace"(
+                    "file" to "src/main/resources/plugin.yml",
+                    "token" to computeFileName(),
+                    "value" to "@VERSION@"
+                )
+            }
+        }
+    }
 }
